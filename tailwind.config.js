389dd79cbcb28d294b2bae51b97be450719ebb2a
Carlos Daniel <deleonguzmancarlosdaniel6@gmail.com>
2024-08-02 16:59:44 -0400

    snap tailwind practi style

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    colores: {
+      'gris': '#A2A6AF',
+      'gris-oscuro': '#585E6C',
+    }
+  },
+};
+export const plugins = [];
+
